a93de40ac93dbde326a14aa7e2dea277343457bd
Divya John <43516829+DivyaJohn@users.noreply.github.com>
2025-12-09 13:39:34 +0530

    Update db.js

--- node_js_crud_operarion/lib/db.js
+++ node_js_crud_operarion/lib/db.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-	host:'localhost',
-	user:'root', //Your Database User Name
-	password:'', // Your Database Password
-	database:'node_js_crud'
+        host:'localhost',
+        user:'nodeuser', //Your Database User Name
+        password:'Pottamplackal@1995', // Your Database Password
+        database:'nodeapp'
 });
 connection.connect(function(error){
-	if(!!error) {
-		console.log(error);
-	} else {
-		console.log('Database Connected Successfully..!!');
-	}
+        if(!!error) {
+                console.log(error);
+        } else {
+                console.log('Database Connected Successfully..!!');
+        }
 });
 
 module.exports = connection;
